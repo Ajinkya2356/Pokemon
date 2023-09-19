3de2369d35aacab7666594dfc0f4a33c395993d9
Ajinkya Jagtap <115774348+Ajinkya2356@users.noreply.github.com>
2023-09-19 18:56:02 +0530

    Initial commit

---
+++ src/components/Poke.js
@@ -0,0 +1,76 @@
+
+import React, { Component } from "react";
+export class Poke extends Component {
+    
+      
+  render() {
+    let {name,imgURL,pid,type} = this.props;
+    return (
+      <div className="my-3 d-flex justify-content-md-between">
+        <div
+          className="card"
+          style={{
+            width: "194px",
+            height: "277px",
+            borderRadius: "8px",
+            border: "1px dashed #2E3156",
+            background: `linear-gradient(180deg, #C0D4C8 0%, ${typeColor[type]} 100%)`,
+            flexShrink: "0",
+          }}
+        >
+          <img src={imgURL} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5
+              className="card-title"
+              style={{
+                color: "#2E3156",
+                textAlign: "center",
+                fontFamily: "Roboto",
+                fontSize: "20px",
+                fontStyle: "normal",
+                fontWeight: "600",
+                lineHeight: "normal",
+              }}
+            >
+              {name}
+            </h5>
+            <p
+              className="card-text"
+              style={{
+                color: "#2E3156",
+                textAlign: "center",
+                fontFamily: "Roboto",
+                fontSize: "20px",
+                fontStyle: "normal",
+                fontWeight: 400,
+                lineHeight: "normal",
+              }}
+            >
+              {pid}
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Poke;
+const typeColor = {
+    normal: "#DDCBD0",
+    fighting: "#FCC1B0",
+    flying: "#B2D2E8",
+    poison: "#CFB7ED",
+    ground: "#F401A6",
+    rock: "#C5AEA8",
+    bug: "#C1E0C8",
+    ghost: "D7C2D7",
+    fire: "#EDC2C4",
+    water: "CBDSED",
+    grass: "C004C8",
+    electric: "#E2E2A0",
+    psychic: "#DDC0CF",
+    ice: "#C7D7DF",
+    dragon: "#CADCDF",
+};
---
+++ src/components/Pokemon.js
@@ -0,0 +1,484 @@
+import React, { Component } from "react";
+import Poke from "./Poke";
+export class Pokemon extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Hello constructor here");
+    this.state = {
+      pokemonData: [],
+      count: 0,
+      page: 1,
+    };
+  }
+  async componentDidMount() {
+    const url = "https://pokeapi.co/api/v2/pokemon";
+    let data = await fetch(url);
+    let passData = await data.json();
+    const pokemonResults = passData.results;
+    const pokemonData = [];
+    for (const pokemon of pokemonResults) {
+      const pokemonDataResponse = await fetch(pokemon.url);
+      const pokemonDetails = await pokemonDataResponse.json();
+      const parts = pokemon.url.split("/");
+      const pokemonId = parts[parts.length - 2];
+      const imageUrl = pokemonDetails.sprites.front_default;
+      const paddedPokemonId = String(pokemonId).padStart(3, "0");
+      const types = pokemonDetails.types.map((typeData) => typeData.type.name);
+      const type = types[0];
+      pokemonData.push({
+        name: pokemon.name,
+        imgURL: imageUrl,
+        pid: paddedPokemonId,
+        type: type,
+      });
+    }
+
+    this.setState({
+      pokemonData: pokemonData,
+      count: passData.count,
+    });
+  }
+  handlePrevious = async () => {
+    if (this.state.page > 1) {
+      const previousPage = this.state.page - 1;
+      const offset = (previousPage - 1) * 20;
+      const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`;
+
+      let data = await fetch(url);
+      let passData = await data.json();
+      const pokemonResults = passData.results;
+      const pokemonData = [];
+      for (const pokemon of pokemonResults) {
+        const pokemonDataResponse = await fetch(pokemon.url);
+        const pokemonDetails = await pokemonDataResponse.json();
+        const parts = pokemon.url.split("/");
+        const pokemonId = parts[parts.length - 2];
+        const imageUrl = pokemonDetails.sprites.front_default;
+        const paddedPokemonId = String(pokemonId).padStart(3, "0");
+        const types = pokemonDetails.types.map(
+          (typeData) => typeData.type.name
+        );
+        const type = types;
+        pokemonData.push({
+          name: pokemon.name,
+          imgURL: imageUrl,
+          pid: paddedPokemonId,
+          type: type,
+        });
+      }
+      this.setState({
+        pokemonData: pokemonData,
+        page: previousPage,
+        count: passData.count,
+      });
+    }
+  };
+  handleNext = async () => {
+    if (this.state.page < Math.ceil(this.state.count / 20)) {
+      const nextPage = this.state.page + 1;
+      const offset = (nextPage - 1) * 20;
+      const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`;
+
+      let data = await fetch(url);
+      let passData = await data.json();
+      const pokemonResults = passData.results;
+      const pokemonData = [];
+      for (const pokemon of pokemonResults) {
+        const pokemonDataResponse = await fetch(pokemon.url);
+        const pokemonDetails = await pokemonDataResponse.json();
+        const parts = pokemon.url.split("/");
+        const pokemonId = parts[parts.length - 2];
+        const imageUrl = pokemonDetails.sprites.front_default;
+        const paddedPokemonId = String(pokemonId).padStart(3, "0");
+        const types = pokemonDetails.types.map(
+          (typeData) => typeData.type.name
+        );
+        const type = types[0];
+        pokemonData.push({
+          name: pokemon.name,
+          imgURL: imageUrl,
+          pid: paddedPokemonId,
+          type: type,
+        });
+      }
+
+      this.setState({
+        pokemonData: pokemonData,
+        page: this.state.page + 1,
+        count: passData.count,
+      });
+    }
+  };
+
+  
+  render() {
+    return (
+      <div className="container py-5">
+        <h1
+          style={{
+            color: "#2E3156",
+            fontFamily: "Roboto",
+            fontSize: "30px",
+            fontStyle: "normal",
+            fontWeight: "700",
+            lineHeight: "normal",
+            letterSpacing: "1.8px",
+          }}
+        >
+          Pokedex |
+          <small
+            style={{
+              color: "#5D5F7E",
+              fontFamily: "Roboto",
+              fontSize: "20px",
+              fontStyle: "normal",
+              fontWeight: "500",
+              lineHeight: "normal",
+            }}
+            className="mx-2"
+          >
+            search for any pokemon that exists on planet
+          </small>
+        </h1>
+
+        <div
+          className="container my-4 d-flex"
+          style={{ background: "#DEEDED", color: "#5D5F7E" }}
+        >
+          <div className="mb-3" style={{ marginLeft: "-10px" }}>
+            <label htmlFor="search" className="form-label">
+              Search by
+            </label>
+
+            <input
+              type="text"
+              className="form-control"
+              id="search"
+              placeholder="search by name"
+              style={{
+                borderRadius: "8px",
+                background: "#C9DDE2",
+                width: "663px",
+                height: "57px",
+              }}
+            />
+          </div>
+          <div className="mb-3 mx-3">
+            <label htmlFor="type" className="form-label">
+              Type
+            </label>
+            <div
+              class="dropdown"
+              style={{
+                borderRadius: "8px",
+                background: "#C9DDE2",
+                width: "194px",
+                height: "57px",
+              }}
+            >
+              <button
+                class="btn dropdown-toggle d-flex align-items-center py-3"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                id="type"
+                style={{ border: "none", color: "#2E3156" }}
+              >
+                Normal+<strong>5 more</strong>
+                <span className="mx-3"></span>
+              </button>
+              <ul
+                class="dropdown-menu px-3 "
+                style={{ width: "194px", color: "#2E3156" }}
+              >
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Normal
+                  <hr className="my-1 mx-2" />
+                </li>
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Fighting
+                  <hr className="my-1 mx-2" />
+                </li>
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Flying
+                  <hr className="my-1 mx-2" />
+                </li>
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Poison
+                  <hr className="my-1 mx-2" />
+                </li>
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Ground
+                  <hr className="my-1 mx-2" />
+                </li>
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Rock
+                  <hr className="my-1 mx-2" />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="mb-3 mx-3">
+            <label htmlFor="gender" className="form-label">
+              Gender
+            </label>
+            <div
+              class="dropdown"
+              style={{
+                borderRadius: "8px",
+                background: "#C9DDE2",
+                width: "194px",
+                height: "57px",
+              }}
+            >
+              <button
+                class="btn dropdown-toggle d-flex align-items-center py-3"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                id="gender"
+                style={{ border: "none", color: "#2E3156" }}
+              >
+                Male+<strong>2 more</strong>
+                <span className="mx-3"></span>
+              </button>
+              <ul
+                class="dropdown-menu"
+                style={{ width: "194px", color: "#2E3156" }}
+              >
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Male
+                  <hr className="my-1 mx-2" />
+                </li>
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Female
+                  <hr className="my-1 mx-2" />
+                </li>
+                <li>
+                  <input className="mx-2" type="checkbox" />
+                  Other
+                  <hr className="my-1 mx-2" />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="mb-3 mx-2">
+            <label htmlFor="stats" className="form-label">
+              Stats
+            </label>
+            <div
+              class="dropdown"
+              style={{
+                borderRadius: "8px",
+                background: "#C9DDE2",
+                width: "194px",
+                height: "57px",
+              }}
+            >
+              <button
+                class="btn dropdown-toggle d-flex align-items-center py-3"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                id="stats"
+                style={{ border: "none", color: "#2E3156" }}
+              >
+                HP+<strong>5 more</strong>
+                <span className="mx-3"></span>
+              </button>
+              <ul
+                class="dropdown-menu"
+                style={{ width: "669px", height: "469px", color: "#2E3156" }}
+              >
+                <h2 className="mx-4 my-2  ">Select Stats</h2>
+                <li className="mx-4 my-4">
+                  <medium style={{ marginRight: "140px" }}>HP</medium>
+                  <div
+                    className="container my-1 mx-5"
+                    style={{
+                      width: "438px",
+                      height: " 31px",
+                      borderRadius: "8px",
+                      border: " 1px solid #2E3156",
+                      background: " #F1F3F3",
+                      position: "absolute",
+                      top: "15%",
+                      left: "20%",
+                    }}
+                  >
+                    <span className="mx-1">0</span>
+                    <input className="py-2" min="0" max="210" type="range" />
+                    210
+                  </div>
+                </li>
+                <li className="mx-4 my-4">
+                  <medium style={{ marginRight: "140px" }}>Attack</medium>
+                  <div
+                    className="container my-1 mx-5"
+                    style={{
+                      width: "438px",
+                      height: " 31px",
+                      borderRadius: "8px",
+                      border: " 1px solid #2E3156",
+                      background: " #F1F3F3",
+                      position: "absolute",
+                      top: "26%",
+                      left: "20%",
+                    }}
+                  >
+                    0
+                    <input type="range" />
+                    210
+                  </div>
+                </li>
+                <li className="mx-4 my-4">
+                  <medium style={{ marginRight: "140px" }}>Defense</medium>
+                  <div
+                    className="container my-1 mx-5"
+                    style={{
+                      width: "438px",
+                      height: " 31px",
+                      borderRadius: "8px",
+                      border: " 1px solid #2E3156",
+                      background: " #F1F3F3",
+                      position: "absolute",
+                      top: "36%",
+                      left: "20%",
+                    }}
+                  >
+                    0
+                    <input type="range" />
+                    210
+                  </div>
+                </li>
+                <li className="mx-4 my-4">
+                  <medium style={{ marginRight: "140px" }}>Speed</medium>
+                  <div
+                    className="container my-1 mx-5"
+                    style={{
+                      width: "438px",
+                      height: " 31px",
+                      borderRadius: "8px",
+                      border: " 1px solid #2E3156",
+                      background: " #F1F3F3",
+                      position: "absolute",
+                      top: "46%",
+                      left: "20%",
+                    }}
+                  >
+                    0
+                    <input type="range" />
+                    210
+                  </div>
+                </li>
+                <li className="mx-4 my-4">
+                  <medium style={{ marginRight: "140px" }}>Sp.Attack</medium>
+                  <div
+                    className="container my-1 mx-5"
+                    style={{
+                      width: "438px",
+                      height: " 31px",
+                      borderRadius: "8px",
+                      border: " 1px solid #2E3156",
+                      background: " #F1F3F3",
+                      position: "absolute",
+                      top: "56%",
+                      left: "20%",
+                    }}
+                  >
+                    0
+                    <input type="range" />
+                    210
+                  </div>
+                </li>
+                <li className="mx-4 my-4">
+                  <medium style={{ marginRight: "140px" }}>Sp.Def</medium>
+                  <div
+                    className="container my-1 mx-5"
+                    style={{
+                      width: "438px",
+                      height: " 31px",
+                      borderRadius: "8px",
+                      border: " 1px solid #2E3156",
+                      background: " #F1F3F3",
+                      position: "absolute",
+                      top: "66%",
+                      left: "20%",
+                    }}
+                  >
+                    0
+                    <input type="range" />
+                    210
+                  </div>
+                </li>
+                <div className="container d-flex justify-content-end">
+                  <button
+                    style={{
+                      borderRadius: "8px",
+                      borderColor: "#2E3156",
+                      color: "#2E3156",
+                    }}
+                    className="btn "
+                  >
+                    Reset
+                  </button>
+                  <button
+                    style={{
+                      borderRadius: "8px",
+                      borderColor: "#2E3156",
+                      backgroundColor: "#2E3156",
+                      color: "white",
+                    }}
+                    className="btn mx-4"
+                  >
+                    Apply
+                  </button>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {this.state.pokemonData.map((element) => {
+            return (
+              <div className="col-md-2" key={element.url}>
+                <Poke
+                  name={element.name ? element.name : ""}
+                  imgURL={element.imgURL}
+                  pid={element.pid}
+                  type={element.type}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            className="btn btn-primary"
+            onClick={this.handlePrevious}
+          >
+            Previous
+          </button>
+          <button
+            disabled={this.state.page + 1 > Math.ceil(this.state.count / 20)}
+            className="btn btn-primary"
+            onClick={this.handleNext}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pokemon;
